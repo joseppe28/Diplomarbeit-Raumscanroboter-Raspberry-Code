3472677c79e444b9f105c62b79707de8a9a3d669
joseppe28 <josef0703.messner@gmail.com>
2026-09-12 11:10:55 +0200

    Range min and max update

--- Lidar/lidar_main.py
+++ Lidar/lidar_main.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+import math
 
 class LidarNode(Node):
 
@@ -29,6 +30,8 @@ class LidarNode(Node):
             msg.intensities,
             msg.angle_min,
             msg.angle_increment,
+            msg.range_min,
+            msg.range_max,
             0  # Replace with actual motor angle if available
         )
 
@@ -41,13 +44,19 @@ class LidarDataConverter():
         pass
 
     # Converts the LIDAR data to a dictionary with teh coordinates and the accuracy 
-    # Parameters: self, the range data, the accuracy of the points,  the minimum / start angel, the angel increment of each scan, the current angle of the motor
+    # Parameters: self, the range data, the accuracy of the points,  the minimum / start angel, the angel increment of each scan, the current angle of the motor, Max and Min range for data check
     # Return: a dictionary with the coordinates and the accuracy of each point
-    def convert_to_3d_Cloud(self, ranges, intensities, angle_min, angle_increment, motor_angle):
+    def convert_to_3d_Cloud(self, ranges, intensities, angle_min, angle_increment, range_min, range_max, motor_angle):
         cloud = {}
+        i = 0
         for r, a in zip(ranges, intensities):
-            angle = angle_min + ranges.index(r) * angle_increment
-             
+
+            angle = angle_min + i * angle_increment
+            i += 1
+
+            if not self.check_data(r, range_min, range_max):
+                continue
+
             # Zuerst ein Dreick in der X-Z Ebende um Z und eine hilfvariable d zu berechnen
             z = r * math.sin(angle)
             d = r * math.cos(angle)
@@ -60,6 +69,12 @@ class LidarDataConverter():
 
         return cloud
 
+    def check_data(self, r, range_min, range_max):
+        if r == 0 or math.isinf(r) or math.isnan(r):
+            return False
+        if r < range_min or r > range_max:
+            return False
+        return True
 
 
 
